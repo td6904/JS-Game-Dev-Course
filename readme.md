5b37776d188d2470f4d5bdae9feef6b7e4b04fdc
TYLER DURNION <td665pro@.pm.me>
2023-01-28 17:01:23 +0100

    Animation from picture provided, pretty cool

---
+++ readme.md
@@ -0,0 +1,3 @@
+Following Video - Cours de développement de jeux JavaScript pour débutants https://www.youtube.com/watch?v=GFO_txvwK_c
+
+At 19m30 - Staggering frames with %, pretty complex stuff!
--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("hello")
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+//Global variable v v v
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 10;
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.fillRect(50,50,100,100);
+    //3, 5 , 9 arguments in this v v v
+    //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage, frameX * spriteWidth,
+         frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+                    //v v v Remainder operator
+         if (gameFrame % staggerFrames == 0){
+         if (frameX < 6) frameX++;
+         else frameX = 0;
+         }
+
+         gameFrame++;
+    requestAnimationFrame(animate);
+};
+animate();
